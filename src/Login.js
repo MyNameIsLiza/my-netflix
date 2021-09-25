c21c09c64bd5fdf49a7c916a82b4024531163662
Tsapyk Yelyzaveta <lizatsapik@gmail.com>
2021-09-25 15:16:21 +0300

    Login form

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,21 @@
 import './Login.css';
+import React from 'react';
 
 function Login() {
     return (
-        <div className="Login">
+        <form className="Login" id="loginForm">
             <div className="loginDiv">
-                <label htmlFor={"loginInput"}>Login</label>
-                <input type="text" id={"loginInput"}/>
+                <label htmlFor="loginInput">Email</label>
+                <input type="email" id="loginInput"/>
             </div>
             <div className="passwordDiv">
-                <label htmlFor={"loginInput"}>Password</label>
-                <input type="password" id={"passwordInput"}/>
+                <label htmlFor="loginInput">Password</label>
+                <input type="password" id="passwordInput"/>
             </div>
-            <input type="submit" value={"Submit"}/>
-        </div>
+            <input type="submit" value="Log In"/>
+        </form>
     );
+
 }
 
 export default Login;
-
