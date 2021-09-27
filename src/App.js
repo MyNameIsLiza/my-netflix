8dc81c228bb16670895df2d6b726d4d59cd70d56
Tsapyk Yelyzaveta <lizatsapik@gmail.com>
2021-09-27 14:26:35 +0300

    Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import React  from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Login from './Login';
 import Users from './Users';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import firebaseConfig from './firebaseConfig';
+firebase.initializeApp(firebaseConfig);
+const authProvider = new firebase.auth.GoogleAuthProvider();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Users/>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState(null);
+
+    const signInWithGoogle = () => {
+        firebase
+            .auth()
+            .signInWithPopup(authProvider)
+            .then(function(result) {
+                console.log(result.credential.accessToken);
+                setUser(result.user);
+            })
+            .catch(error => {
+                console.error({
+                    ...error
+                });
+            });
+    };
+
+    const signOut = () => {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => {
+                setUser(null);
+            })
+            .catch(error => {
+                console.error({
+                    ...error
+                });
+            });
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello CodeSandbox</h1>
+                <h2>Start editing to see some magic happen!</h2>
+                {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+                {user ? (
+                    <button onClick={signOut}>Sign out</button>
+                ) : (
+                    <button onClick={signInWithGoogle}>Sign in with Google</button>
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/Users.js
+++ src/Users.js
@@ -1,7 +1,6 @@
 import './Users.css';
 import React, {useState, useEffect} from 'react';
 
-
 function Users() {
     const [users, setUsers] = useState(0);
     useEffect(() => {
@@ -20,19 +19,16 @@ function Users() {
     return (
         <ul className="Users">{users}</ul>
     );
-};
+}
 
 function User(U) {
     return (
         <li className="User">{U.name}
-            {U.image?.medium ? <img src={U.image?.medium} alt=""/> :
+            {U.image?.medium ? <img src={U.image?.medium} alt="user photo"/> :
                 <img
                     src="https://st3.depositphotos.com/9998432/19048/v/600/depositphotos_190484900-stock-illustration-default-placeholder-businessman-half-length.jpg"
-                    alt=""/>}
+                    alt="no photo"/>}
         </li>
     );
 }
-
-;
-
 export default Users;
